aea6fba46051d69aed0745c17b4c5b9bb57e1612
Luke Lee <luke0201@naver.com>
2018-03-31 18:08:01 +0900

    Add training parameter in dropout layers

--- model.py
+++ model.py
@@ -6,9 +6,10 @@ bnorm = tf.layers.batch_normalization
 dropout = tf.layers.dropout
 
 relu = tf.nn.relu
-relu = tf.nn.leaky_relu
+lrelu = tf.nn.leaky_relu
 sigmoid = tf.sigmoid
 tanh = tf.tanh
+softmax = tf.nn.softmax
 
 def generator(Z, training):
     with tf.variable_scope('generator'):
@@ -29,15 +30,15 @@ def discriminator(X, training, fake=False):
     with tf.variable_scope('discriminator', reuse=fake):
         fc1 = fc(X, 1024, name='hidden1')
         fc1 = relu(fc1)
-        fc1 = dropout(fc1, 0.3)
+        fc1 = dropout(fc1, 0.3, training=training)
 
         fc2 = fc(fc1, 512, name='hidden2')
         fc2 = relu(fc2)
-        fc2 = dropout(fc2, 0.3)
+        fc2 = dropout(fc2, 0.3, training=training)
 
         fc3 = fc(fc2, 256, name='hidden3')
         fc3 = relu(fc3)
-        fc3 = dropout(fc3, 0.3)
+        fc3 = dropout(fc3, 0.3, training=training)
 
         output = fc(fc3, 1, name='output')
         output = sigmoid(output)
