ab5e42542d1539f35fe918a6c346847378fee2b2
Luke Lee <luke0201@naver.com>
2018-04-01 07:01:31 +0900

    Use TensorFlow's image encoding

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 from tqdm import tqdm
 
-from PIL import Image
+#from PIL import Image
 import numpy as np
 import tensorflow as tf
 
@@ -15,8 +15,15 @@ img_output_dir = 'output'
 gan_checkpoint_path = os.path.join('checkpoint', 'final-1522487574.854423')
 
 Z = tf.placeholder(tf.float32, [batch_size, latent_size], name='Z')
-
 g_z = model.generator(Z, training=False)
+
+with tf.name_scope('output'):
+    Single_z = tf.placeholder(tf.float32, [784], name='Single_z')
+    left, right = tf.reduce_min(Single_z), tf.reduce_max(Single_z)
+    make_png = tf.subtract(Single_z, tf.constant(left, tf.float32, [784]))
+    make_png = tf.scalar_mul(right - left, make_png)
+    make_png = tf.cast(make_png, tf.uint8)
+    make_png = tf.image.encode_png(make_png)
 
 g_var = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
 saver = tf.train.Saver(var_list=g_var)
@@ -31,13 +38,12 @@ with tf.Session() as sess:
     def save_images(imgs, name, offset):
         for i, img in enumerate(imgs):
             # Transform image
-            img = img.reshape([28, 28])
-            left, right = img.min(), img.max()
-            img = (255. / (right - left)) * (img - np.full(img.shape, left))
-            img = img.astype(np.uint8)
+            png_encoded = sess.run(make_png, feed_dict={Single_Z: img})
 
-            Image.fromarray(img, mode='L').save(os.path.join(
-                    img_output_dir, '{}{:04d}.png'.format(name, i + offset)))
+            file_path = os.path.join(
+                    img_output_dir, '{}{:04d}.png'.format(name, i + offset))
+            with open(file_path, 'wb') as f:
+                f.write(png_encoded)
 
     for i in tqdm(range(0, n_data, batch_size)):
         Z_batch = np.random.normal(size=[batch_size, latent_size])
